51da18304bdc13ada733528076861ac6811d4021
kcirekcom <erick.f.mock@gmail.com>
2017-01-25 22:58:56 -0800

    used require.context to add ng construct defs

--- app/entry.js
+++ app/entry.js
@@ -10,3 +10,14 @@ const angular = require('angular');
 const uiRouter = require('angular-ui-router');
 
 const routesApp = angular.module('routesApp', [uiRouter]);
+
+let context = require.context('./config/', true, /\.js$/);
+context.keys().forEach(key => {
+  routesApp.config(context(key));
+});
+
+context = require.context('./view/', true, /\.js$/);
+context.keys().forEach(key => {
+  let name = pascalcase(path.basename(key, '.js'));
+  routesApp.controller(name, context(key));
+});
